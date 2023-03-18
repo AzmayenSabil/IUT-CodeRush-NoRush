b9440ab3fad1b59245ba8e46f3bdda775905b718
Raiyan-Noor <raiyannoor@iut-dhaka.edu>
2023-03-18 21:22:08 +0600

    skills, experiences, summary separated as components

--- client/src/components/Resume/cv.js
+++ client/src/components/Resume/cv.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Sidebar from "./sidebar";
 import Skills from "./skills";
-
+import Summary from "./summary";
+import Experience from "./experience";
+import Projects from "./projects";
 import '../../assets/css/orbit-1.css'
 
 import "../../assets/plugins/bootstrap/css/bootstrap.min.css"
@@ -13,74 +15,11 @@ function Cv(){
   <div className="wrapper mt-lg-5">
     <Sidebar/>
     <div className="main-wrapper">
-      <section className="section summary-section">
-        <h2 className="section-title"><span className="icon-holder"><i className="fa-solid fa-user" /></span>Career Profile</h2>
-        <div className="summary">
-          <p>Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can <a href="https://themes.3rdwavemedia.com/bootstrap-templates/resume/orbit-free-resume-cv-bootstrap-theme-for-developers/" target="_blank">download this free resume/CV template here</a>. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.</p>
-        </div>{/*//summary*/}
-      </section>{/*//section*/}
-      <section className="section experiences-section">
-        <h2 className="section-title"><span className="icon-holder"><i className="fa-solid fa-briefcase" /></span>Experiences</h2>
-        <div className="item">
-          <div className="meta">
-            <div className="upper-row">
-              <h3 className="job-title">Lead Developer</h3>
-              <div className="time">2022 - Present</div>
-            </div>{/*//upper-row*/}
-            <div className="company">Startup Hubs, San Francisco</div>
-          </div>{/*//meta*/}
-          <div className="details">
-            <p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.</p>  
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
-          </div>{/*//details*/}
-        </div>{/*//item*/}
-        <div className="item">
-          <div className="meta">
-            <div className="upper-row">
-              <h3 className="job-title">Senior Software Engineer</h3>
-              <div className="time">2018 - 2022</div>
-            </div>{/*//upper-row*/}
-            <div className="company">Google, London</div>
-          </div>{/*//meta*/}
-          <div className="details">
-            <p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>  
-          </div>{/*//details*/}
-        </div>{/*//item*/}
-        <div className="item">
-          <div className="meta">
-            <div className="upper-row">
-              <h3 className="job-title">UI Developer</h3>
-              <div className="time">2016 - 2018</div>
-            </div>{/*//upper-row*/}
-            <div className="company">Amazon, London</div>
-          </div>{/*//meta*/}
-          <div className="details">
-            <p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>  
-          </div>{/*//details*/}
-        </div>{/*//item*/}
-      </section>{/*//section*/}
-      <section className="section projects-section">
-        <h2 className="section-title"><span className="icon-holder"><i className="fa-solid fa-archive" /></span>Projects</h2>
-        <div className="intro">
-          <p>You can list your side projects or open source libraries in this section. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et ligula in nunc bibendum fringilla a eu lectus.</p>
-        </div>{/*//intro*/}
-        <div className="item">
-          <span className="project-title"><a href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/coderpro-bootstrap-5-startup-template-for-software-projects/" target="_blank">CoderPro</a></span> - <span className="project-tagline">A responsive website template designed to help developers launch their software projects. </span>
-        </div>{/*//item*/}
-        <div className="item">
-          <span className="project-title"><a href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/launch-bootstrap-5-template-for-saas-businesses/" target="_blank">Launch</a></span> - <span className="project-tagline">A responsive website template designed to help startups promote their products or services.</span>
-        </div>{/*//item*/}
-        <div className="item">
-          <span className="project-title"><a href="https://themes.3rdwavemedia.com/bootstrap-templates/resume/devcard-bootstrap-5-vcard-portfolio-template-for-software-developers/" target="_blank">DevCard</a></span> - <span className="project-tagline">A portfolio website template designed for software developers.</span>
-        </div>{/*//item*/}
-        <div className="item">
-          <span className="project-title"><a href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/bootstrap-template-for-mobile-apps-nova-pro/" target="_blank">Nova Pro</a></span> - <span className="project-tagline">A responsive Bootstrap theme designed to help app developers promote their mobile apps</span>
-        </div>{/*//item*/}
-        <div className="item">
-          <span className="project-title"><a href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-web-development-agencies-devstudio/" target="_blank">DevStudio</a></span> - 
-          <span className="project-tagline">A responsive website template designed to help web developers/designers market their services. </span>
-        </div>{/*//item*/}
-      </section>{/*//section*/}
+      <Summary/>
+      <Experience/>
+      
+      <Projects/>
+      
       <Skills/>
     </div>{/*//main-body*/}
   </div>
