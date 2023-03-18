59eeb7a969bcea88e5db8a12c2cfadc11a2b704a
Raiyan-Noor <raiyannoor@iut-dhaka.edu>
2023-03-18 21:03:55 +0600

    Skills section separated as component

--- client/src/components/Resume/cv.js
+++ client/src/components/Resume/cv.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Sidebar from "./sidebar";
+import Skills from "./skills";
 
 import '../../assets/css/orbit-1.css'
+
+import "../../assets/plugins/bootstrap/css/bootstrap.min.css"
 
 function Cv(){
     return(
@@ -78,47 +81,7 @@ function Cv(){
           <span className="project-tagline">A responsive website template designed to help web developers/designers market their services. </span>
         </div>{/*//item*/}
       </section>{/*//section*/}
-      <section className="skills-section section">
-        <h2 className="section-title"><span className="icon-holder"><i className="fa-solid fa-rocket" /></span>Skills &amp; Proficiency</h2>
-        <div className="skillset">        
-          <div className="item">
-            <h3 className="level-title">Python &amp; Django</h3>
-            <div className="progress level-bar">
-              <div className="progress-bar theme-progress-bar" role="progressbar" style={{width: '99%'}} aria-valuenow={99} aria-valuemin={0} aria-valuemax={100} />
-            </div>                               
-          </div>{/*//item*/}
-          <div className="item">
-            <h3 className="level-title">Javascript</h3>
-            <div className="progress level-bar">
-              <div className="progress-bar theme-progress-bar" role="progressbar" style={{width: '98%'}} aria-valuenow={98} aria-valuemin={0} aria-valuemax={100} />
-            </div>                              
-          </div>{/*//item*/}
-          <div className="item">
-            <h3 className="level-title">React &amp; Angular</h3>
-            <div className="progress level-bar">
-              <div className="progress-bar theme-progress-bar" role="progressbar" style={{width: '98%'}} aria-valuenow={98} aria-valuemin={0} aria-valuemax={100} />
-            </div>                                 
-          </div>{/*//item*/}
-          <div className="item">
-            <h3 className="level-title">HTML5 &amp; CSS</h3>
-            <div className="progress level-bar">
-              <div className="progress-bar theme-progress-bar" role="progressbar" style={{width: '95%'}} aria-valuenow={95} aria-valuemin={0} aria-valuemax={100} />
-            </div>                                
-          </div>{/*//item*/}
-          <div className="item">
-            <h3 className="level-title">Ruby on Rails</h3>
-            <div className="progress level-bar">
-              <div className="progress-bar theme-progress-bar" role="progressbar" style={{width: '85%'}} aria-valuenow={85} aria-valuemin={0} aria-valuemax={100} />
-            </div>                                  
-          </div>{/*//item*/}
-          <div className="item">
-            <h3 className="level-title">Sketch &amp; Photoshop</h3>
-            <div className="progress level-bar">
-              <div className="progress-bar theme-progress-bar" role="progressbar" style={{width: '60%'}} aria-valuenow={60} aria-valuemin={0} aria-valuemax={100} />
-            </div>                                 
-          </div>{/*//item*/}
-        </div>  
-      </section>{/*//skills-section*/}
+      <Skills/>
     </div>{/*//main-body*/}
   </div>
 
