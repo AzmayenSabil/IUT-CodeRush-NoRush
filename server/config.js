3b9d1702c6efdbdedd55affc1d71ed5cbc930eee
Azmayen Fayek Sabil <azmayenfayek@iut-dhaka.edu>
2023-03-18 09:14:40 +0600

    config update

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ export default {
     JWT_SECRET: "Omd8ZV6RqvtLmmJrlfSrtgYxGo/DyyvDZ1n0u4Kch07c=",
     EMAIL: "eugenia.pollich@ethereal.email",
     PASSWORD: "eAs4aNcNZkMzznfexF",
-    ATLAS_URI: "mongodb+srv://admin:admin@cluster0.zz76rf7.mongodb.net/?retryWrites=true&w=majority"
+    ATLAS_URI: "mongodb+srv://admin:admin@hackathon.ri7jjua.mongodb.net/?retryWrites=true&w=majority"
 }
